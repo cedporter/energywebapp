797318c64ecd1bc2472e5f6ca1adbb661c670e45
Edward Porter <ed.porter.iii@gmail.com>
2016-11-18 09:58:41 -0500

    Apparently all the routes go in the index.js file?

--- routes/currentstatus.js
+++ routes/currentstatus.js
@@ -1,20 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/currentstatus', function (req, res){
-  var options = {
-    method: "GET",
-    uri: access_url + "/switches",
-    headers: {
-      'Authorization': 'Bearer' + bearer
-    }
-  };
-
-  request(options, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-          res.send(body)
-       }
-  })
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,20 @@ router.get('/', function (req, res){
   });
 });
 
+router.get('/currentstatus', function (req, res){
+  var options = {
+    method: "GET",
+    uri: access_url + "/switches",
+    headers: {
+      'Authorization': 'Bearer' + bearer
+    }
+  };
+
+  request(options, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+          res.send(body)
+       }
+  })
+});
+
 module.exports = router;
