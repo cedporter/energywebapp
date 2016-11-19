b3d7b2dfb3295c2b5d21de6df39832e7fccd6a70
Edward Porter <ed.porter.iii@gmail.com>
2016-11-19 10:32:32 -0500

    still trying

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,7 @@ router.get('/currentstatus', function (req, res){
 
   request(options, function (error, response, body) {
       if (!error && response.statusCode == 200) {
-        console.log(body)
+        console.log(body + "\nType: " typeof body)
         res.render('currentstatus', {
           "itemlist" : body
       });
