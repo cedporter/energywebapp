912507d453ededb4c57096c56511d6b8786e9ca8
Edward Porter <ed.porter.iii@gmail.com>
2016-11-20 12:23:38 -0500

    Durations is the collection we’ll want to format and display.

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res){
   var db = req.db;
-  db.collection('switch_test').find().toArray(function(err, docs) {
+  db.collection('durations').find().toArray(function(err, docs) {
     console.log("RETRIEVED " + docs);
     if(err) throw err;
     res.send(docs);
