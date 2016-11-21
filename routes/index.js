43f0fd36a1f64aefec52d5b9c7914291badc8a73
aditi-sharma <aditisharma.b@gmail.com>
2016-11-21 00:27:29 -0500

    Edit 34 in index.jade, index.js

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,6 @@ router.get('/', function (req, res){
   var db = req.db;
   db.collection('durations').find().toArray(function(err, docs) {
     console.log("RETRIEVED " + docs);
-    docs = docs.JSON;
     if(err) throw err;
     res.render('index', {"results" : docs
     });
